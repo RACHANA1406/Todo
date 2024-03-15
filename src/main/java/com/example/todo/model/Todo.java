609813801e8016882bc127a3dbd0e8e9f3ace7aa
Rachana Ekkandolla <123464644+RACHANA1406@users.noreply.github.com>
2024-03-15 23:21:20 +0530

    Update Todo.java

--- src/main/java/com/example/todo/model/Todo.java
+++ src/main/java/com/example/todo/model/Todo.java
@@ -2,7 +2,9 @@ package com.example.todo.model;
 
 public class Todo {
     private int id;
-    private String todo, priority, status;
+    private String todo;
+    private String priority;
+    private String status;
 
     public Todo(int id, String todo, String priority, String status) {
         this.id = id;
@@ -27,14 +29,6 @@ public class Todo {
         this.todo = todo;
     }
 
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
     public String getPriority() {
         return priority;
     }
@@ -42,4 +36,13 @@ public class Todo {
     public void setPriority(String priority) {
         this.priority = priority;
     }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
+
